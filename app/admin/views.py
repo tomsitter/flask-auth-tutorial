8dd34d870f548552914811ce37ad867fd0a3630d
Tom Sitter <thomas.sitter@gmail.com>
2021-12-13 22:01:09 -0800

    Add admin function

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,14 +1,50 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash, abort, request
 from functools import wraps
-from app.auth.views import login_required, current_user
-from models import User, Role
+from app.auth.views import current_user, admin_required, login_required
+from app.models import User, Gig
+from app import db
 
-admin = Blueprint("admin", __name__, template_folder='/templates')
+admin = Blueprint("admin", __name__, template_folder='templates')
 
-def admin_required(f):
-    @wraps(f)
-    def _admin_required(f):
-        if current_user and current_user.is_admin():
+@admin.before_request
+@login_required
+@admin_required
+def check_admin_in_each_view():
+    print(f"Admin {current_user.username} access {request.url}")
+
+@admin.route('/gigs')
+def gigs():
+    gigs = Gig.query.all()
+    return render_template('all_gigs.html', gigs=gigs)
+
+@admin.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('all_users.html', users=users)
+
+@admin.route('/delete_gig/<int:gig_id>', methods=['POST'])
+def delete_gig(gig_id):
+    gig = Gig.query.get(gig_id)
+    if gig:
+        flash(f"Gig \"{gig.title}\" is deleted", "success")
+        db.session.delete(gig)
+        db.session.commit()
+    
+    if "gig/info" in request.referrer:
+        return redirect(url_for("admin.gigs"))
+    else:
+        return redirect(request.referrer)
 
 
-    return _admin_required(f)
+@admin.route('/delete_user/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        flash(f"USer \"{user.username}\" is deleted", "success")
+        db.session.delete(user)
+        db.session.commit()
+    
+    if "user/profile" in request.referrer:
+        return redirect(url_for("admin.users"))
+    else:
+        return redirect(request.referrer)
