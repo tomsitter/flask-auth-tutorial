45afb27dc6b6fa1bfc1862392fc786f9aeb59a7b
Thomas Sitter <tomsitter@Thomass-MacBook-Pro.local>
2021-12-21 09:16:29 -0800

    Fix admin_required decorator

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from functools import wraps
-from app.auth.views import login_required, current_user
-from models import User, Role
+from app.auth.views import current_user
+from app.models import User, Role
 
 admin = Blueprint("admin", __name__, template_folder='/templates')
 
 def admin_required(f):
     @wraps(f)
-    def _admin_required(f):
-        if current_user and current_user.is_admin():
-
-
+    def _admin_required(*args, **kwargs):
+        if not current_user.is_admin():
+            flash("You need to be admin to access this page", "danger")
+            return redirect(url_for("main.home"))
+        return f(*args, **kwargs)
     return _admin_required(f)
